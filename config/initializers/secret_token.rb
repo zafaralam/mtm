210c26130c6ea326a9524ee458299d4006f8ac56
zafar <fastalam420@gmail.com>
2013-10-16 14:28:46 +1100

    created new project for My Task Manangment(MTM)

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Mtm::Application.config.secret_key_base = 'c163753884c7b676bf871bcb79c3b7d16000ac1ba0ee77e406700008af469f0fa07649db3e710619443826aec8ebccb0ca25656447e6ecf920e51cad06dd53ac'
